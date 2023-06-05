0ec7c9efb2f341d49253fec616903e0a6d782cb6
jude <63959832+blackhammersudo@users.noreply.github.com>
2023-06-05 22:28:35 +0530

    done2

--- src/App.js
+++ src/App.js
@@ -18,10 +18,10 @@ function App() {
       {/* <Createpost /> */}
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Body />} />
-          <Route path='/Login' element={<Loginpage />} />
-          <Route path='/Registation' element={<Registation />} />
-          <Route path='/Createpost' element={<Createpost />} />
+          <Route path='/redditpro' element={<Body />} />
+          <Route path='/redditpro/Login' element={<Loginpage />} />
+          <Route path='/redditpro/Registation' element={<Registation />} />
+          <Route path='/redditpro/Createpost' element={<Createpost />} />
         </Routes>
       </BrowserRouter>
       <Fotterb />
